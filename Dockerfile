231d7580a65d138f6da895d1684c4828cf510b91
Francesco Cheinasso <cheinasso.francesco@gmail.com>
2022-07-21 17:21:52 +0200

    added reset flag

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,7 @@ ENV POSTGRES_USER=postgres
 ENV POSTGRES_DB=postgres 
 
 COPY ./scripts/* /docker-entrypoint-initdb.d/
+COPY ./docker-entrypoint.sh ./docker-entrypoint.sh
+RUN chmod +x ./docker-entrypoint.sh
 
+ENTRYPOINT [ "/docker-entrypoint.sh" ]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ $# -gt 0 ]; then
+    for arg in "$@"; do
+        case $arg in
+            --reset)
+                echo "RESETTING DATABASE ..."
+                rm -rf /var/lib/postgresql/data/*
+            ;;
+        esac
+    done
+fi
+
+
+exec /usr/local/bin/docker-entrypoint.sh postgres
+
