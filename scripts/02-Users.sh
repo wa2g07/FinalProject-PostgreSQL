c79cbee5a9be059727c7a8e7b50992721251975c
Marco Manco <50114357+ralls0@users.noreply.github.com>
2022-08-29 17:54:32 +0200

    Update 02-Users.sh

--- scripts/02-Users.sh
+++ scripts/02-Users.sh
@@ -13,6 +13,6 @@ echo 'Created table users'
 
 psql -U "$POSTGRES_USER" -c '
 insert into users (id, activated, email, password, roles, username) 
-values (1, TRUE, '"'"'admin'"'"', '"'"'$2a$12$Sq2r0MiAzhog1ApiwT/DWe6JS8Nert8v27EjObfbK5GRu2LrIV12G'"'"', '"'"'SUPERADMIN'"'"', '"'"'admin'"'"');' "$POSTGRES_LOGIN_SERVICE_DB"
+values (0, TRUE, '"'"'admin'"'"', '"'"'$2a$12$Sq2r0MiAzhog1ApiwT/DWe6JS8Nert8v27EjObfbK5GRu2LrIV12G'"'"', '"'"'SUPERADMIN'"'"', '"'"'admin'"'"');' "$POSTGRES_LOGIN_SERVICE_DB"
 echo 'Added admin user'
 
